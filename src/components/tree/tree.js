d372a69d3f22a395ffb43ebd664107848dcf41e8
Troy Blank <troy@troyblank.com>
2019-10-10 15:07:16 -0500

    built out dom tree with Node component recursion

--- src/components/tree/tree.js
+++ src/components/tree/tree.js
@@ -1,14 +1,23 @@
 import React, { useEffect } from 'react';
 import { getListing } from './actions';
+import { Node } from '..';
+import './tree.scss';
 
 export default function Tree({ dispatch, treeStore = {} }) {
-    const { listing = [] } = treeStore;
+    const { listing } = treeStore;
 
     useEffect(() => {
         dispatch(getListing());
     }, []);
 
-    return (
-      <div>{listing.toString()}</div>
-    );
+    // This assumes there is only one root node.
+    if (listing) {
+        return (
+          <ul className={'tree'}>
+            <Node structure={listing[0]} />
+          </ul>
+        );
+    }
+
+    return null;
 }
--- src/components/tree/tree.spec.js
+++ src/components/tree/tree.spec.js
@@ -5,16 +5,20 @@ import { assert } from 'chai';
 import { shallow } from 'enzyme';
 import { render } from '@testing-library/react';
 import Tree from './tree';
+import { Node } from '..';
 
 describe('Tree', () => {
     const chance = new Chance();
 
     it('should render', () => {
-        const listing = chance.word();
+        const structure = chance.word();
+        const listing = [structure];
         const wrapper = shallow(<Tree dispatch={sinon.spy()} treeStore={{ listing }} />);
 
         assert.isTrue(wrapper.contains(
-          <div>{listing}</div>
+          <ul className={'tree'}>
+            <Node structure={structure} />
+          </ul>
         ));
     });
 
